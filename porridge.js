d5ac0b35c3544db5d0a133192f245e39484a5e7b
Leiloukou <jacobaleger12@gmail.com>
2022-03-08 19:59:02 -0500

    Removed the docs, and renamed the variables

--- porridge.js
+++ porridge.js
@@ -1,29 +1,29 @@
 // The following code is licensed by the APACHE LICENSE 2.0, in the LICENSE file of this repository or in the parent folder of this file.
 
 /**
- * This is the object you will be working with while using parc.
- * The parc object will contain all of the functions you will use from parc.
- * Warning: Don't assign any variable with a name that starts with the word "parc".
+ * This is the object you will be working with while using porridge.
+ * The porridge object will contain all of the functions you will use from porridge.
+ * Warning: Don't assign any variable with a name that starts with the word "porridge".
  *
  * @global
  * @format
  * @constant
  * @type {object}
- * @version 1.0.0 This is the current version of parc that you are viewing.
- * @property {object} localStorage This is the localStorage object in parc.
- * @property {object} object This is the object object in parc.
- * @property {object} array This is the array object in parc.
- * @property {function} help This is the help function in parc.
+ * @version 1.0.0 This is the current version of porridge that you are viewing.
+ * @property {object} localStorage This is the localStorage object in porridge.
+ * @property {object} object This is the object object in porridge.
+ * @property {object} array This is the array object in porridge.
+ * @property {function} help This is the help function in porridge.
  */
 
-let parc = {};
+let porridge = {};
 
 /**
  * This is the localStorage object.
  * This includes all the localStorage methods and also methods like preserving an item, and getting all the localStorage items.
  *
  * @global
- * @memberof parc
+ * @memberof porridge
  * @type {object}
  * @constant
  * @property {function} setItem This sets an item into the browser's localStorage.
@@ -34,22 +34,22 @@ let parc = {};
  * @property {function} wipe This function wipes localStorage, including preservedItems.
  */
 
-parc.localStorage = {};
+localPorridge = {};
 
 /**
  * This sets an item into the browser's localStorage.
  *
  * @global
  * @type {function}
- * @memberof parc.localStorage
- * @method parc.localStorage.setItem
- * @param {string} parcItemName
- * @param {*} parcItemValue
+ * @memberof localPorridge
+ * @method localPorridge.setItem
+ * @param {string} porridgeItemName
+ * @param {*} porridgeItemValue
  * @constant
  */
 
-parc.localStorage.setItem = (parcItemName, parcItemValue) => {
-    localStorage.setItem(parcItemName, parcItemValue);
+localPorridge.setItem = (porridgeItemName, porridgeItemValue) => {
+    localStorage.setItem(porridgeItemName, porridgeItemValue);
 };
 
 /**
@@ -57,24 +57,24 @@ parc.localStorage.setItem = (parcItemName, parcItemValue) => {
  *
  * @global
  * @type {function}
- * @memberof parc.localStorage
- * @method parc.localStorage.getItem
- * @param {string} parcItemName
+ * @memberof localPorridge
+ * @method localPorridge.getItem
+ * @param {string} porridgeItemName
  * @returns {*}
  * @constant
  */
 
-parc.localStorage.getItem = (parcItemName) => {
-    if (localStorage.getItem(parcItemName) === null) {
+localPorridge.getItem = (porridgeItemName) => {
+    if (localStorage.getItem(porridgeItemName) === null) {
         if (
-            localStorage.getItem(`parcPreservedItem:-${parcItemName}`)
+            localStorage.getItem(`porridgePreservedItem:-${porridgeItemName}`)
         ) {
             return localStorage.getItem(
-                `parcPreservedItem:-${parcItemName}`
+                `porridgePreservedItem:-${porridgeItemName}`
             );
         };
     } else {
-        return localStorage.getItem(parcItemName);
+        return localStorage.getItem(porridgeItemName);
     };
 };
 
@@ -83,74 +83,74 @@ parc.localStorage.getItem = (parcItemName) => {
  *
  * @global
  * @type {function}
- * @memberof parc.localStorage
- * @method parc.localStorage.getAll
+ * @memberof localPorridge
+ * @method localPorridge.getAll
  * @returns {array}
  * @constant
  */
 
-parc.localStorage.getAll = () => {
-    let parcResult = [];
+localPorridge.getAll = () => {
+    let porridgeResult = [];
     Object.keys(JSON.parse(JSON.stringify(localStorage))).forEach(
-        (parcLocalStorageItem) => {
-            parcResult.push({
-                name: parcLocalStorageItem.replace(
-                    'parcPreservedItem:-',
+        (porridgeLocalStorageItem) => {
+            porridgeResult.push({
+                name: porridgeLocalStorageItem.replace(
+                    'porridgePreservedItem:-',
                     ''
                 ),
                 value: localStorage.getItem(
-                    parcLocalStorageItem.replace(
-                        'parcPreservedItem:-',
+                    porridgeLocalStorageItem.replace(
+                        'porridgePreservedItem:-',
                         ''
                     )
                 )
             });
         }
     );
-    return parcResult;
+    return porridgeResult;
 };
 
 /**
  * This function preserves an currently in localStorage.
  * If the item is not in localStorage, it will be added as a new item preserved item.
- * A preserved item will not be cleared from localStorage running parc.localStorage.clear().
- * If you want to clear all the items from localStorage you can use parc.localStorage.wipe().
+ * A preserved item will not be cleared from localStorage running localPorridge.clear().
+ * If you want to clear all the items from localStorage you can use localPorridge.wipe().
  *
  * @global
  * @type {function}
- * @memberof parc.localStorage
- * @method parc.localStorage.preserveItem
- * @param {string} parcItemName
+ * @memberof localPorridge
+ * @method localPorridge.preserveItem
+ * @param {string} porridgeItemName
  * @constant
  */
 
-parc.localStorage.preserveItem = (parcItemName, parcItemValue) => {
-    if (localStorage.getItem(parcItemName) !== null) {
-        localStorage.removeItem(parcItemName);
+localPorridge.preserveItem = (porridgeItemName, porridgeItemValue) => {
+    if (localStorage.getItem(porridgeItemName) !== null) {
+        localStorage.removeItem(porridgeItemName);
     }
     localStorage.setItem(
-        `parcPreservedItem:-${parcItemName}`,
-        parcItemValue
+        `porridgePreservedItem:-${porridgeItemName}`,
+        porridgeItemValue
     );
 };
 
 /**
  * This function will clear all items in localStorage, excluding any preserved items.
- * A preserved item will not be cleared from localStorage running parc.localStorage.clear().
- * If you want to clear all the items from localStorage you can use parc.localStorage.wipe().
+ * A preserved item will not be cleared from localStorage running localPorridge.clear().
+ * If you want to clear all the items from localStorage you can use localPorridge.wipe().
  *
  * @global
  * @type {function}
- * @memberof parc.localStorage
- * @method parc.localStorage.clear
+ * @memberof localPorridge
+ * @method localPorridge.clear
  * @constant
  */
 
-parc.localStorage.clear = () => {
+localPorridge.clear = () => {
     Object.keys(JSON.parse(JSON.stringify(localStorage))).forEach(
-        (parcItemName) => {
-            if (/parcPreservedItem:-/.test(parcItemName) === false) {
-                localStorage.removeItem(parcItemName);
+        (porridgeItemName) => {
+            if (/porridgePreservedItem:-/.test(porridgeItemName) === false) {
+                localStorage.removeItem(porridgeItemName);
             }
         }
     );
@@ -158,121 +158,121 @@ parc.localStorage.clear = () => {
 
 /**
  * This function clears all the items from localStorage, including preserved items.
- * If you dont want to clear any preserved items, you can use parc.localStorage.clear() instead.
+ * If you dont want to clear any preserved items, you can use localPorridge.clear() instead.
  *
  * @global
  * @type {function}
- * @memberof parc.localStorage
- * @method parc.localStorage.wipe
+ * @memberof localPorridge
+ * @method localPorridge.wipe
  * @constant
  */
 
-parc.localStorage.wipe = () => {
+localPorridge.wipe = () => {
     localStorage.clear();
 };
 
 /**
  * This is an an object with multiple functions with methods you can use to edit objects.
- * This is kind of like the regular Javascript high level array methods, or the new ES6 object methods, but parc has much more features.
- * Another benefit of using parc is that even with all these fast, new, easy to use functions, you can still use every Javascript feature out there, including the var keyword.
+ * This is kind of like the regular Javascript high level array methods, or the new ES6 object methods, but porridge has much more features.
+ * Another benefit of using porridge is that even with all these fast, new, easy to use functions, you can still use every Javascript feature out there, including the var keyword.
  */
 
-parc.object = {};
+porridge.object = {};
 
 /**
  * This function will turn an object into an array.
  *
  * @global
- * @param {object} parcObject The object to convert to array.
+ * @param {object} porridgeObject The object to convert to array.
  * @returns {array} The returned array of converted from the passed object.
  */
 
-parc.object.arrayify = (parcObject) => {
-    const parcArray = [];
+porridge.object.arrayify = (porridgeObject) => {
+    const porridgeArray = [];
 
-    for (let i = 0; i < Object.keys(parcObject).length; i++) {
-        parcArray.push({
-            key: Object.keys(parcObject)[i],
-            value: Object.values(parcObject)[i]
+    for (let i = 0; i < Object.keys(porridgeObject).length; i++) {
+        porridgeArray.push({
+            key: Object.keys(porridgeObject)[i],
+            value: Object.values(porridgeObject)[i]
         });
     };
 
-    return parcArray;
+    return porridgeArray;
 };
 
 /**
  * This function will turn an object into an array.
  *
  * @global
- * @param {object} parcObject The object to convert to array.
+ * @param {object} porridgeObject The object to convert to array.
  * @returns {array} The returned array of converted from the passed object.
  */
 
-parc.object.toArray = (parcObject) => {
-    return parc.object.arrayify(parcObject);
+porridge.object.toArray = (porridgeObject) => {
+    return porridge.object.arrayify(porridgeObject);
 };
 
 /**
  * This is an an object with multiple functions with methods you can use to edit arrays.
- * This is kind of like the regular Javascript high level array methods, or the new ES6 object methods, but parc has much more features.
- * Another benefit of using parc is that even with all these fast, new, easy to use functions, you can still use every Javascript feature out there, including the var keyword.
+ * This is kind of like the regular Javascript high level array methods, or the new ES6 object methods, but porridge has much more features.
+ * Another benefit of using porridge is that even with all these fast, new, easy to use functions, you can still use every Javascript feature out there, including the var keyword.
  */
 
-parc.array = {};
+porridge.array = {};
 
 /**
  * This function turns an array into an object.
  * 
  * @global
- * @param {array} parcArray 
+ * @param {array} porridgeArray 
  * @returns {object}
  */
 
-parc.array.objectify = (parcArray) => {
-	return Object.entries(parcArray);
+porridge.array.objectify = (porridgeArray) => {
+	return Object.entries(porridgeArray);
 };
 
 /**
  * This function turns an array into an object.
  * 
  * @global
- * @param {array} parcArray 
+ * @param {array} porridgeArray 
  * @returns {object}
  */
 
-parc.array.toObject = (parcArray) => {
-	return Object.entries(parcArray);
+porridge.array.toObject = (porridgeArray) => {
+	return Object.entries(porridgeArray);
 };
 
 /**
  * This function turns an array into a string.
  *
  * @global
- * @param {array} parcArray This is the array that will be turned into a string.
+ * @param {array} porridgeArray This is the array that will be turned into a string.
  * @returns {string} Returns the passed array, as a string.
  */
 
-parc.array.stringify = (parcArray) => {
-    return parcArray.toString();
+porridge.array.stringify = (porridgeArray) => {
+    return porridgeArray.toString();
 };
 
 /**
  * This function turns an array into a string.
  *
  * @global
- * @param {array} parcArray This is the array that will be turned into a string.
+ * @param {array} porridgeArray This is the array that will be turned into a string.
  * @returns {string} Returns the passed array, as a string.
  */
 
-parc.array.toString = (parcArray) => {
-    return parcArray.toString();
+porridge.array.toString = (porridgeArray) => {
+    return porridgeArray.toString();
 };
 
 /**
- * This function logs the parc object to the console.
- * This function is mainly used for beginners trying to use or learn parc.js.
+ * This function logs the porridge object to the console.
+ * This function is mainly used for beginners trying to use or learn porridge.js.
  */
 
-parc.help = () => {
-    console.log('parc.help() has returned: \n', parc);
+porridge.help = () => {
+    console.log('porridge.help() has returned: \n', porridge);
 };
