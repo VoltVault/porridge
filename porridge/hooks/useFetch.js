bcb2dfdfd7aca0b49afc8fb89aaf65604efb9555
Lil' Wuth <jacobaleger12@gmail.com>
2022-03-14 12:25:58 -0400

    Create useFetch.js

---
+++ porridge/hooks/useFetch.js
@@ -0,0 +1,50 @@
+/** @format */
+
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * @param {string} porridgeUrl
+ * @param {object} porridgeOptions
+ */
+export default function useFetch(porridgeUrl, porridgeOptions) {
+	if (!porridgeUrl) throw new Error('The first parameter is required.');
+	if (typeof porridgeUrl != 'string')
+		throw new Error('The first parameter must be a string.');
+
+	const [porridgeData, porridgeSetData] = useState(null);
+	const [porridgeError, porridgeSetError] = useState(null);
+	const [porridgeLoading, porridgeSetLoading] = useState(null);
+
+	useEffect(() => {
+		let porridgeIsMounted;
+		porridgeIsMounted = true;
+
+		porridgeSetLoading(true);
+
+		fetch(porridgeUrl, porridgeOptions)
+			.then((porridgeRes) => porridgeRes.json())
+			.then((porridgeData) => {
+				if (porridgeIsMounted) {
+					porridgeSetData(porridgeData);
+					porridgeSetError(null);
+				}
+			})
+			.catch((porridgeError) => {
+				porridgeSetError(porridgeError);
+			})
+			.finally(() => porridgeIsMounted && porridgeSetLoading(false));
+	}, [porridgeUrl, porridgeOptions]);
+
+	return { porridgeLoading, porridgeError, porridgeData };
+}
+
+useFetch.defaultProps = {
+	porridgeUrl: 'https://randomuser.me/api/',
+	porridgeOptions: null
+};
+
+useFetch.propTypes = {
+	porridgeUrl: PropTypes.string.isRequired,
+	porridgeOptions: PropTypes.object
+};
